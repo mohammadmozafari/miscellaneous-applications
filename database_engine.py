a21833a24a1746ee7dbc3aa251df9139da6a6506
mohammad <mozafari.mmd@gmail.com>
2020-01-25 22:32:11 +0330

    changed database structure

--- database_engine.py
+++ database_engine.py
@@ -29,6 +29,7 @@ class Log_Item(pv.Model):
         database = db
 
 class Delivery(pv.Model):
+    national_code = pv.IntegerField(primary_key=True)
     first_name = pv.CharField(null=False)
     last_name = pv.CharField(null=False)
     phone_number = pv.CharField(null=False, constraints=[pv.Check('phone_number REGEXP \'^([0-9]\{11\})$\'')])
@@ -41,7 +42,7 @@ class Log_Delivery(pv.Model):
         database = db
 
 class Client(pv.Model):
-    id = pv.IntegerField(primary_key=True)
+    national_code = pv.IntegerField(primary_key=True)
     first_name = pv.CharField(null=False)
     last_name = pv.CharField(null=False)
     phone_number = pv.CharField(null=False, constraints=[pv.Check('phone_number REGEXP \'^([0-9]\{11\})$\'')])
@@ -55,15 +56,17 @@ class Log_Client(pv.Model):
         database = db
 
 class Address(pv.Model):
-    client_id = pv.ForeignKeyField(Client, null=False)
+    national_code = pv.ForeignKeyField(Client, null=False)
     name = pv.CharField(null=False)
     address = pv.CharField(null=False)
     phone_number = pv.CharField(null=False, constraints=[pv.Check('phone_number REGEXP \'^([0-9]\{11\})$\'')])
     class Meta:
+        primary_key = pv.CompositeKey('national_code', 'name')
         database = db
 class Log_Address(pv.Model):
     access_type = pv.CharField(null=False)
-    record_id = pv.IntegerField(null=False)
+    national_code = pv.IntegerField(null=False)
+    name = pv.CharField(null=False)
     class Meta:
         database = db
 
@@ -89,8 +92,8 @@ class Item_Receipt(pv.Model):
         database = db
 class Log_Item_Receipt(pv.Model):
     access_type = pv.CharField(null=False)
-    record1_id = pv.IntegerField(null=False)
-    record2_id = pv.IntegerField(null=False)
+    item_id = pv.IntegerField(null=False)
+    receipt_id = pv.IntegerField(null=False)
     class Meta:
         database = db
 
@@ -102,8 +105,8 @@ class Item_Market(pv.Model):
         database = db
 class Log_Item_Market(pv.Model):
     access_type = pv.CharField(null=False)
-    record1_id = pv.IntegerField(null=False)
-    record2_id = pv.IntegerField(null=False)
+    item_id = pv.IntegerField(null=False)
+    market_id = pv.IntegerField(null=False)
     class Meta:
         database = db
 
@@ -142,44 +145,30 @@ def create_table(table_name):
     if table_name in tables:
         db.drop_tables([model])
     db.create_tables([model])
-    # if table_name == 'client' or table_name == 'delivery' or table_name == 'address':
-    #     db.execute_sql("ALTER TABLE %s ADD CONSTRAINT phone_number_check CHECK(phone_number REGEXP '^([0-9]\{11\})$')" % table_name)
 
 def add_triggers():
-    first_query =  ("Create Trigger {0}"
-                  " AFTER {1} ON {2}"
-                  " FOR EACH ROW"
-                  " BEGIN"
-                  " INSERT INTO log_{3}(access_type, record_id) VALUES(\'{4}\', {5}.id);"
-                  " END")
-
-    second_query = ("Create Trigger {0}"
-                   " AFTER {1} ON {2}_{3}"
-                   " FOR EACH ROW"
-                   " BEGIN"
-                   " INSERT INTO log_{4}_{5}(access_type, record1_id, record2_id) VALUES(\'{6}\', {7}.{8}_id, {9}.{10}_id);"
-                   " END")
+    first_query =  'Create Trigger {0} AFTER {1} ON {2} FOR EACH ROW BEGIN INSERT INTO log_{2}(access_type, record_id) VALUES(\'{1}\', {3}); END'
+    second_query = 'Create Trigger {0} AFTER {1} ON {2} FOR EACH ROW BEGIN INSERT INTO log_{2}(access_type, {3}, {4}) VALUES(\'{1}\', {5}, {6}); END'
     tables = [
-        'market',
-        'item',
-        'client',
-        'address',
-        'delivery',
-        'receipt',
+        ('market', 'id'),
+        ('item', 'id'),
+        ('client', 'national_code'),
+        ('delivery', 'national_code'),
+        ('receipt', 'id'),
     ]
-    for table in tables:
-        db.execute_sql(first_query.format('insert_'+table, 'insert', table, table, 'insert', 'NEW'))
-        db.execute_sql(first_query.format('update_'+table, 'update', table, table, 'update', 'OLD'))
-        db.execute_sql(first_query.format('delete_'+table, 'delete', table, table, 'delete', 'OLD'))
-
+    for t in tables:
+        db.execute_sql(first_query.format('insert_'+t[0], 'insert', t[0], 'NEW.'+t[1]))
+        db.execute_sql(first_query.format('update_'+t[0], 'update', t[0], 'OLD.'+t[1]))
+        db.execute_sql(first_query.format('delete_'+t[0], 'delete', t[0], 'OLD.'+t[1]))
     tables = [
-        ('item', 'market'),
-        ('item', 'receipt')
+        ('item_market', 'item_id', 'market_id'),
+        ('item_receipt', 'item_id', 'receipt_id'),
+        ('address', 'national_code_id', 'name')
     ]
-    for t1, t2 in tables:
-        db.execute_sql(second_query.format('insert_'+t1+'_'+t2, 'insert', t1, t2, t1, t2, 'insert', 'NEW', t1, 'NEW', t2))
-        db.execute_sql(second_query.format('update_'+t1+'_'+t2, 'update', t1, t2, t1, t2, 'update', 'OLD', t1, 'OLD', t2))
-        db.execute_sql(second_query.format('delete_'+t1+'_'+t2, 'delete', t1, t2, t1, t2, 'delete', 'OLD', t1, 'OLD', t2))
+    for t in tables:
+        db.execute_sql(second_query.format('insert_'+t[0], 'insert', t[0], t[1], t[2], 'NEW.'+t[1], 'NEW.'+t[2]))
+        db.execute_sql(second_query.format('update_'+t[0], 'update', t[0], t[1], t[2], 'OLD.'+t[1], 'OLD.'+t[2]))
+        db.execute_sql(second_query.format('delete_'+t[0], 'delete', t[0], t[1], t[2], 'OLD.'+t[1], 'OLD.'+t[2]))
 
 def print_markets():
     print('-----------------')
@@ -197,6 +186,9 @@ def print_deliveries():
     print(Delivery.select().count(None), 'delivery[s] found.')
     print('-----------------')
 
+def print_foods():
+    print('-----------------')
+
 def change_database(function, **args):
     try:
         function(**args)
@@ -204,3 +196,5 @@ def change_database(function, **args):
         print('something went wrong :(')
         return
     print('database updated :)')
+
+setup_database()
