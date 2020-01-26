1d981922894635a721d26708a837bb3b038a33f9
mohammad <mozafari.mmd@gmail.com>
2020-01-27 02:48:44 +0330

    printing cleanup done

--- database_engine.py
+++ database_engine.py
@@ -208,9 +208,10 @@ def add_store_precedure():
     db.execute_sql(query)
              
 def print_markets():
-    print('-----------------')
+    print('-----------------\n')
+    print('| {:>3} | {:<15} | {:<6} |'.format('id', 'name', 'active'))
     for x in Market.select().execute(None):
-        print('{:>3} | {:<15} | {}'.format(x.id, x.name, x.active))
+        print('| {:>3} | {:<15} | {:<6} |'.format(x.id, x.name, x.active))
     print()
     x = Market.select().count(None)
     print(x, 'market[s] found.')
@@ -218,9 +219,10 @@ def print_markets():
     return x
             
 def print_deliveries():
-    print('-----------------')
+    print('-----------------\n')
+    print('| {:>15} | {:<30} | {:<11} |'.format('national code', 'name', 'phone'))
     for x in Delivery.select().execute(None):
-        print('{:>3} | {:<30} | {}'.format(x.national_code, x.first_name + ' ' + x.last_name, x.phone_number))
+        print('| {:>15} | {:<30} | {:<11} |'.format(x.national_code, x.first_name + ' ' + x.last_name, x.phone_number))
     print()
     x = Delivery.select().count(None)
     print(x, 'delivery[s] found.')
@@ -228,9 +230,10 @@ def print_deliveries():
     return x
 
 def print_items():
-    print('-----------------')
+    print('-----------------\n')
+    print('| {:>3} | {:<15} | {:>5} | {:<5} |'.format('id', 'name', 'price', 'food?'))
     for x in Item.select().where(Item.end_time == '2100-01-01 09:00:00').execute(None):
-        print('{:>3} | {:<15} | {:>5} | {}'.format(x.id, x.name, x.price, x.food))
+        print('| {:>3} | {:<15} | {:>5} | {:<5} |'.format(x.id, x.name, x.price, x.food))
     print()
     x = Item.select().where(Item.end_time == '2100-01-01 09:00:00').count(None)
     print(x, 'item[s] found.')
@@ -238,9 +241,10 @@ def print_items():
     return x
 
 def print_foods():
-    print('-----------------')
+    print('-----------------\n')
+    print('| {:>3} | {:<15} | {:>5} | {:<11} |'.format('id', 'name', 'price', 'food'))
     for x in Item.select().where(Item.food, Item.end_time == '2100-01-01 09:00:00').execute(None):
-        print('{:>3} | {:<15} | {:>5} | {}'.format(x.id, x.name, x.price, x.food))
+        print('| {:>3} | {:<15} | {:>5} | {:<11} |'.format(x.id, x.name, x.price, x.food))
     print()
     x = Item.select().where(Item.food, Item.end_time == '2100-01-01 09:00:00').count(None)
     print(x, 'food[s] found.')
@@ -248,9 +252,10 @@ def print_foods():
     return x
 
 def print_materials():
-    print('-----------------')
+    print('-----------------\n')
+    print('| {:>3} | {:<15} | {:<5} |'.format('id', 'name', 'price'))
     for x in Item.select().where(not Item.food, Item.end_time == '2100-01-01 09:00:00').execute(None):
-        print('{:>3} | {:<15} | {:>5}'.format(x.id, x.name, x.price))
+        print('| {:>3} | {:<15} | {:>5} |'.format(x.id, x.name, x.price))
     print()
     x = Item.select().where(not Item.food, Item.end_time == '2100-01-01 09:00:00').count(None)
     print(x, 'material[s] found.')
@@ -258,9 +263,10 @@ def print_materials():
     return x
 
 def print_clients():
-    print('-----------------')
+    print('-----------------\n')
+    print('| {:>15} | {:<30} | {:<11} | {:<3} |'.format('national code', 'name', 'phone', 'age'))
     for x in Client.select().execute(None):
-        print('{} | {:<30} | {} | {}'.format(x.national_code, x.first_name + ' ' + x.last_name, x.phone_number, dt.datetime.now().year - x.birth_year))
+        print('| {:>15} | {:<30} | {:<11} | {:<3} |'.format(x.national_code, x.first_name + ' ' + x.last_name, x.phone_number, dt.datetime.now().year - x.birth_year))
     print()
     x = Client.select().count(None)
     print(x, 'client[s] found.')
@@ -268,9 +274,10 @@ def print_clients():
     return x
 
 def print_addresses(client):
-    print('-----------------')
+    print('-----------------\n')
+    print('| {:<15} | {:<30} | {:<11} |'.format('name', 'address', 'phone'))
     for x in Address.select().where(Address.national_code_id == client).execute(None):
-        print('{:<15} | {:<50} | {}'.format(x.name, x.address, x.phone_number))
+        print('| {:<15} | {:<30} | {:<11} |'.format(x.name, x.address, x.phone_number))
     print()
     x = Address.select().where(Address.national_code_id == client).count(None)
     print(x, 'address\'[s] found.')
@@ -278,9 +285,10 @@ def print_addresses(client):
     return x
 
 def print_market_materials(market):
-    print('-----------------')
+    print('-----------------\n')
+    print('| {:>3} | {:<15} | {:>5} |'.format('id', 'name', 'price'))
     for x in Item.select().join(Item_Market).where(Item_Market.market_id == market).execute(None):
-        print('{:>3} | {:<15} | {:>5}'.format(x.id, x.name, x.price))
+        print('| {:>3} | {:<15} | {:>5} |'.format(x.id, x.name, x.price))
     print()
     x = Item.select().join(Item_Market).where(Item_Market.market_id == market).count(None)
     print(x, 'material[s] found.')
@@ -288,38 +296,49 @@ def print_market_materials(market):
     return x
 
 def print_client_reports(client):
-    total = 0
-    print('-----------------')
     query = ("SELECT receipt.id, receipt.address, item.name, item.price"
             " FROM receipt"
             " INNER JOIN item_receipt ON receipt.id = item_receipt.receipt_id"
             " INNER JOIN item ON item.id = item_receipt.item_id"
             " WHERE receipt.client_id = " + str(client))
-
-    count = 0
+    query2 = ("SELECT item.name, count(*) as times"
+              " FROM receipt"
+              " INNER JOIN item_receipt ON receipt.id = item_receipt.receipt_id"
+              " INNER JOIN item ON item.id = item_receipt.item_id"
+              " WHERE receipt.client_id = '" + str(client) + "' GROUP BY item.food ORDER BY times DESC LIMIT 1")
+    fav_food = None
+    for x in db.execute_sql(query2):
+        fav_food = x[0]
+    
+    print('-----------------\n')
+    print('| {:>3} | {:<50} | {:<10} | {:<10} |'.format('id', 'address', 'food name', 'food price'))
+    count, total = 0, 0
     for x in db.execute_sql(query):
-        print('{:>3} | {:<7} | {:<50} | {}'.format(x[0], x[1], x[2], x[3]))
+        print('| {:>3} | {:<50} | {:<10} | {:<10} |'.format(str(x[0]), str(x[1]), str(x[2]), str(x[3])))
         total += x[3]
         count += 1
+    
     print()
     x = count
     print(x, 'item[s] found')
     print('total cost:', total)
+    print('favorite food:', fav_food)
     print('-----------------')
     return x
 
 def print_manager_reports():
     total = 0
-    print('-----------------')
+    print('-----------------\n')
     query = ("SELECT receipt.id, item.price, receipt.address, item.food"
             " FROM receipt" 
             " INNER JOIN item_receipt ON receipt.id = item_receipt.receipt_id"
             " INNER JOIN item ON item.id = item_receipt.item_id"
             " WHERE (receipt.at > DATE_SUB(now(), INTERVAL 1 DAY));")
 
+    print('| {:>3} | {:<7} | {:<50} | {:<10} |'.format('id', 'price', 'address', 'is food'))
     query_result = db.execute_sql(query)
     for x in query_result:
-        print('{:>3} | {:<7} | {}'.format(x[0], x[1], x[2]))
+        print('| {:>3} | {:<7} | {:<50} | {:<10} |'.format(x[0], x[1], str(x[2]), x[3]))
         if x[3]:
             total += x[1]
         else:
