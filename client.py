85e0af54dcd038304179be96734cdf84c512e8db
mohammad <mozafari.mmd@gmail.com>
2020-06-25 19:51:26 +0430

    network communication

--- client.py
+++ client.py
@@ -1,66 +1,83 @@
 import socket
 import threading
 import os
+import time
 
-DIS_PORT_SEND = 1441
-DIS_PORT_RECV = 1440
-DIS_FILE = 'cluster_nodes.txt'
+print_lock = threading.Lock()
 
 def main():
-    a = {'x': '11', 'y': '22'}
-    print((serialize_dic(a)))
-    # discovery_recv = threading.Thread(target=recv_nodes, args=(DIS_PORT_RECV, DIS_FILE))
-    # discovery_send = threading.Thread(target=send_nodes, args=(DIS_PORT_SEND, DIS_FILE))
+    dis_listen_port = 1446
+    dis_file_path = 'list.nw'
+    dis_interval = 5
 
-def read_cluster_file(file):
+    discovery_recv = threading.Thread(target=recv_nodes, args=(dis_listen_port, dis_file_path))
+    discovery_recv.start()
+
+    discovery_send = threading.Thread(target=send_nodes, args=(dis_listen_port, dis_file_path, dis_interval))
+    discovery_send.start()
+
+def read_cluster_file(path):
     cluster = {}
-    if not os.path.exists(file):
-        return None
-    with open(file, 'r') as f:
+    if not os.path.exists(path):
+        return cluster
+    with open(path, 'r') as f:
         lines = f.readlines()
         for line in lines:
-            addr, name = line.split(' ')
-            name = name[:-1]
-            cluster[addr] = name
+            l = line.strip()
+            name, addr = l.split(' ')
+            cluster[name] = addr
     return cluster
 
-def update_cluster_file(new_cluster, file):
-    if os.path.exists(file):
-        cluster = read_cluster_file(file)
-        cluster.update(new_cluster)
-    else:
-        cluster = new_cluster
-    with open(file, 'w') as f:
+def update_cluster_file(new_cluster, path):
+    cluster = read_cluster_file(path)
+    cluster.update(new_cluster)
+    with open(path, 'w') as f:
         for x, y in cluster.items():
-            f.write(y + ' ' + x + '\n')
+            if x == socket.gethostname():
+                continue
+            f.write(x + ' ' + y + '\n')
 
 def deserialize_string(st):
     st = st.strip()
     cluster = {}
-    lines = st.split(' ')
-    for line in lines:
-        print(line)
-        name, addr = line.split(',')
-        cluster[addr] = name
+    nodes = st.split(' ')
+    for node in nodes:
+        name, addr = node.split(',')
+        cluster[name] = addr
     return cluster
 
 def serialize_dic(dic):
     st = ''
     for x, y in dic.items():
-        st += (y + ',' + x + ' ')
+        st += (x + ',' + y + ' ')
     return st.strip()
 
-def recv_nodes():
+def recv_nodes(port, path):
     sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sck.bind(('', port))
+    print_clean('DISC RECV: Listening ...')
     while True:
         data, addr = sck.recvfrom(1024)
+        data = data.decode('utf-8')
         cluster = deserialize_string(data)
-        update_cluster_file(cluster, DIS_FILE)
-        print('discovery message recieved and merged.')
+        cluster.update({socket.gethostbyaddr(addr[0])[0]: addr[0]})
+        update_cluster_file(cluster, path)
+        print_clean('DISC RECV: Discovery message received.')
 
-def send_nodes():
+def send_nodes(port, path, interval):
+    sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
-        pass
+        cluster = read_cluster_file(path)
+        data = serialize_dic(cluster)
+        for name, addr in cluster.items():
+            sck.sendto(data.encode('utf-8'), (addr, port))
+            print_clean('DISC SEND: Sent dis message to ' + addr)
+        time.sleep(interval)
+
+def print_clean(msg):
+    print_lock.acquire()
+    print(msg)
+    print_lock.release()
 
 if __name__ == "__main__":
     main()
