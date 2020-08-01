7775617c0719e3407386ba8e1d068100ff5e813f
mohammad <mozafari.mmd@gmail.com>
2020-08-01 19:44:56 +0430

    Add process_get

--- client.py
+++ client.py
@@ -2,21 +2,32 @@ import socket
 import threading
 import os
 import time
+import datetime
 
 print_lock = threading.Lock()
+send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_port = 0
+tcp_port = 0
+list_file = 'list.txt'
+folder = 'files'
 
 def main():
-    global udp_port
+    global udp_port, list_file, folder
     
     udp_port = int(input('Enter UDP port: '))
-    path = 'list.txt'
     dis_interval = 5
 
-    discovery_recv = threading.Thread(target=recv_nodes, args=(udp_port, path))
+    discovery_recv = threading.Thread(target=udp_recv, args=(udp_port, list_file))
     discovery_recv.start()
-    discovery_send = threading.Thread(target=send_nodes, args=(udp_port, path, dis_interval))
+    discovery_send = threading.Thread(target=send_nodes, args=(udp_port, list_file, dis_interval))
     discovery_send.start()
+
+    while True:
+        command = input()
+        
+        
+        if command == 'exit':
+            return
 
 
 def read_file(path):
@@ -59,27 +70,55 @@ def build_message(dic):
         st += '{} {}\n'.format(port, ip)
     return st.strip()
 
-def recv_nodes(port, path):
-    sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sck.bind(('', port))
-    print_clean('DISC RECV: Listening ...')
-    while True:
-        data, addr = sck.recvfrom(1024)
-        data = data.decode('utf-8')
-        cluster, port = extract_data(data)
-        cluster.update({port: addr[0]})
-        update_file(cluster, path)
-        print_clean('DISC RECV: Discovery message received.')
-
 def send_nodes(port, path, interval):
-    sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    global send_socket
     while True:
         cluster = read_file(path)
         data = build_message(cluster)
         for port, ip in cluster.items():
-            sck.sendto(data.encode('utf-8'), (ip, port))
-            print_clean('DISC SEND: Sent dis message to ' + ip + ':' + str(port))
+            send_socket.sendto(data.encode('utf-8'), (ip, port))
+            # print_clean('DISC SEND: Sent dis message to ' + ip + ':' + str(port))
         time.sleep(interval)
+
+def udp_recv(port, path):
+    sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sck.bind(('', port))
+    print_clean('DISC RECV: Listening ...')
+    while True:
+        msg, addr = sck.recvfrom(1024)
+        msg = msg.decode('utf-8')
+        msg_type = msg[:4]
+        
+        if msg_type == 'Disc':
+            cluster, port = extract_data(msg)
+            cluster.update({port: addr[0]})
+            update_file(cluster, path)
+
+        elif msg_type == 'Gett':
+            process_get(msg, addr)
+
+        elif msg_type == 'Resp':
+            pass
+        else:
+            print_clean('Message format not supported.')
+
+def process_get(msg, source):
+    global send_socket
+    msg = msg.strip()[5:]
+    filename, destination_port = msg.split(' ')
+    destination_port = int(destination_port)
+    files = os.listdir('./path')
+    for file in files:
+        if file == filename:
+            msg = build_get_response(filename)
+            send_socket.sendto(msg.encode('utf-8'), (source[0], destination_port))
+            return
+
+def build_get_response(filename):
+    global tcp_port
+    now = datetime.datetime.now()
+    msg = 'Resp {}\n{}'.format(tcp_port, now)
+    return msg            
 
 def print_clean(msg):
     print_lock.acquire()
@@ -87,4 +126,15 @@ def print_clean(msg):
     print_lock.release()
 
 if __name__ == "__main__":
-    main()
+    # print(datetime.datetime.now())
+    t1 = datetime.datetime.now()
+    time.sleep(0.2)
+    t2 = datetime.datetime.now()
+
+    t0 = '2020-08-01 19:27:06.615044'
+
+    d = datetime.datetime.strptime(t0, '%Y-%m-%d %H:%M:%S.%f')
+
+    print(d)
+    print(t2 - d)
+    # main()
